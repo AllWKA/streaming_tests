caefcf01da77754fd4eba6212e17bb93bbd31bde
Bryan Jaramillo <bryan.jaramillo@bitingbit.com>
2020-04-29 14:30:36 +0100

    WIP: serving using buffer

--- src/api/index.js
+++ src/api/index.js
@@ -4,98 +4,49 @@ const fs = require('fs');
 // const cors = require('cors');
 const config = require('./apiConfig');
 
-const {Readable} = require('stream');
-const util = require('util');
-
 // app.use(cors());
 // app.use(express.json());
-
-const MyStream = function(options) {
-  Readable.call(this, options);
-  this.counter = 1000;
-};
-
-util.inherits(MyStream, Readable);
-
-MyStream.prototype._read = function(n) {
-  this.push('foobar');
-  if (this.counter-- === 0) {
-    this.push(null);
-  }
-};
 
 app.listen(config.port, () => {
   console.log('listening on port: 3000');
 });
 
 app.get('/resource', (req, res) => {
-  console.log('ask for new resource');
+  console.log('new request --> header:', req.headers);
   const path = JSON.parse(fs.readFileSync('./src/api/apiConfig.json')).path;
-  const stat = fs.statSync(path);
-  const fileSize = stat.size;
+  // const stat = fs.statSync(path);
+  // const fileSize = stat.size;
+  let middleBuffer = Buffer.alloc(0);
   const file = fs.createReadStream(path);
 
-  const head = {
-    'Content-Length': fileSize,
-    'Content-Type': 'audio/*'
-  };
-  res.writeHead(200, head);
-  let chunksSend = 0;
-  // let raw_data = Buffer.alloc(0);
-
-  // file._read = () => {};
-
   file.on('data', (chunk) => {
-    const id = chunksSend++;
-    console.log('There will be no additional data for ' + (500 * id) + ' second for:', id);
-    // if (id<15){
-      setTimeout(() => {
-        console.log('resume for:', id);
-        res.write(chunk);
-      }, 2000 * id);
-    // }
+    console.log('file data:', chunk.length);
+    middleBuffer = Buffer.concat([middleBuffer, chunk]);
+    // file.push(chunk);
+    // res.write(chunk);
   });
 
   file.on('end', () => {
-    console.log('______There will be no more data.______');
+    console.log(middleBuffer.length);
+    for (let i = 0; i < middleBuffer.length; i++) {
+      setTimeout(()=>{
+        if (i%500==0){
+          console.log('serving 500:', i);
+        }
+        res.write(Buffer.from([middleBuffer[i]]));
+      }, 5*i);
+    }
   });
 
-  // file.read();
-  // file.on('readable', () => {
-  //   chunksSend++;
-  //   // file.pause();
-  //   console.log('There will be no additional data for ' + (500 * chunksSend) + ' second for:', chunksSend);
-  //   setTimeout(() => {
-  //     console.log('resume for:', chunksSend);
-  //     // file.resume();
-  //     file.read();
-  //   }, 500 * chunksSend);
-  // });
+  // const head = {
+  //   'Content-Length': fileSize,
+  //   'Content-Type': 'audio/*'
+  // };
+  // res.writeHead(200, head);
 
-  // file.pipe(res);
-
-  // if (range) {
-  //   const parts = range.replace(/bytes=/, '').split('-');
-  //   const start = parseInt(parts[0], 10);
-  //   const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-  //   const chunkSize = (end - start) + 1;
-  //   const file = fs.createReadStream(path, {start, end});
-  //   const head = {
-  //     'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-  //     'Accept-Ranges': 'bytes',
-  //     'Content-Length': chunkSize,
-  //     'Content-Type': 'video/mp4'
-  //   };
-  //   res.writeHead(206, head);
-  //   file.pipe(res);
-  // } else {
-  //   const head = {
-  //     'Content-Length': fileSize,
-  //     'Content-Type': 'video/mp4'
-  //   };
-  //   res.writeHead(200, head);
-  //   fs.createReadStream(path).pipe(res);
-  // }
+  file.on('end', () => {
+    console.log('______No more data.______');
+  });
 });
 
 app.get('/path', (req, res) => {
