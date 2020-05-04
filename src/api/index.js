67f1f951b26f3591f1950432ce03f1cc7bd0040a
Bryan Jaramillo <bryan.jaramillo@bitingbit.com>
2020-05-04 13:11:09 +0100

    sendind chunks using headers request

--- src/api/index.js
+++ src/api/index.js
@@ -1,101 +1,58 @@
-// const express = require('express');
+const express = require('express');
 const app = require('express')();
 const fs = require('fs');
-// const cors = require('cors');
+const cors = require('cors');
 const config = require('./apiConfig');
 
-const {Readable} = require('stream');
-const util = require('util');
+app.use(cors());
+app.use(express.json());
 
-// app.use(cors());
-// app.use(express.json());
-
-const MyStream = function(options) {
-  Readable.call(this, options);
-  this.counter = 1000;
-};
-
-util.inherits(MyStream, Readable);
-
-MyStream.prototype._read = function(n) {
-  this.push('foobar');
-  if (this.counter-- === 0) {
-    this.push(null);
-  }
-};
+var simulateLatency = require('express-simulate-latency');
+// use as middleware for all subsequent handlers...
+var smallLag = simulateLatency({min: 100, max: 500});
+app.use(smallLag);
+// ...or use as middleware for a specific route
+var bigLag = simulateLatency({min: 10000, max: 10000});
 
 app.listen(config.port, () => {
   console.log('listening on port: 3000');
 });
 
-app.get('/resource', (req, res) => {
-  console.log('ask for new resource');
+app.get('/resource', bigLag, (req, res) => {
+  console.log('new request');
   const path = JSON.parse(fs.readFileSync('./src/api/apiConfig.json')).path;
   const stat = fs.statSync(path);
   const fileSize = stat.size;
-  const file = fs.createReadStream(path);
+  const range = req.headers.range;
 
-  const head = {
-    'Content-Length': fileSize,
-    'Content-Type': 'audio/*'
-  };
-  res.writeHead(200, head);
-  let chunksSend = 0;
-  // let raw_data = Buffer.alloc(0);
+  console.log('range:', range);
 
-  // file._read = () => {};
-
-  file.on('data', (chunk) => {
-    const id = chunksSend++;
-    console.log('There will be no additional data for ' + (500 * id) + ' second for:', id);
-    // if (id<15){
-      setTimeout(() => {
-        console.log('resume for:', id);
-        res.write(chunk);
-      }, 2000 * id);
-    // }
-  });
-
-  file.on('end', () => {
-    console.log('______There will be no more data.______');
-  });
-
-  // file.read();
-  // file.on('readable', () => {
-  //   chunksSend++;
-  //   // file.pause();
-  //   console.log('There will be no additional data for ' + (500 * chunksSend) + ' second for:', chunksSend);
-  //   setTimeout(() => {
-  //     console.log('resume for:', chunksSend);
-  //     // file.resume();
-  //     file.read();
-  //   }, 500 * chunksSend);
-  // });
-
-  // file.pipe(res);
-
-  // if (range) {
-  //   const parts = range.replace(/bytes=/, '').split('-');
-  //   const start = parseInt(parts[0], 10);
-  //   const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-  //   const chunkSize = (end - start) + 1;
-  //   const file = fs.createReadStream(path, {start, end});
-  //   const head = {
-  //     'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-  //     'Accept-Ranges': 'bytes',
-  //     'Content-Length': chunkSize,
-  //     'Content-Type': 'video/mp4'
-  //   };
-  //   res.writeHead(206, head);
-  //   file.pipe(res);
-  // } else {
-  //   const head = {
-  //     'Content-Length': fileSize,
-  //     'Content-Type': 'video/mp4'
-  //   };
-  //   res.writeHead(200, head);
-  //   fs.createReadStream(path).pipe(res);
-  // }
+  if (range) {
+    console.log('there is range');
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunkSize = (end - start) + 1;
+    const file = fs.createReadStream(path, {start, end});
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'audio/*'
+    };
+    console.log('head:', head);
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    console.log('no range');
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4'
+    };
+    res.writeHead(200, head);
+    console.log('head:', head);
+    fs.createReadStream(path).pipe(res);
+  }
 });
 
 app.get('/path', (req, res) => {
